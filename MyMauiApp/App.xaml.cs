8b3be082765152d49117fceb43d19df756baa97a
Niek Rossen <niek.rossen@infosupport.com>
2024-04-25 15:28:24 +0200

    inital commit

---
+++ MyMauiApp/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMauiApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        // When the app is forced to use a specific theme, e.g. Dark
+        // The OS components like keyboard, timepicker, etc. will use theme of the OS.
+        // I would expect the OS component to use the theme of the app.
+        UserAppTheme = AppTheme.Dark;
+
+        MainPage = new AppShell();
+    }
+}
